2b3aa545f2abed78cebfd924fba0a60caaa90f28
JInsunnya <jsk5754@naver.com>
2025-10-06 23:26:23 +0900

    work

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -10,17 +12,22 @@ public class App {
 //        scanner.close();
 
         try (Scanner scanner = new Scanner(System.in)) {
+            List<Todo> todos = new ArrayList<>();
+
             while (true) {
                 System.out.print("명령) ");
                 String cmd = scanner.nextLine().trim();
+                long todosLastId = 0;
 
                 if (cmd.equals("exit")) break;
                 else if (cmd.equals("add")) {
-                    long id = 1;
+                    long id = todosLastId + 1;
                     System.out.print("할일 : ");
                     String content = scanner.nextLine().trim();
 
                     Todo todo = new Todo(id, content);
+                    todos.add(todo);
+                    todosLastId++;
 
                     System.out.printf("%d번 할일이 생성되었습니다.\n", id);
                 }
